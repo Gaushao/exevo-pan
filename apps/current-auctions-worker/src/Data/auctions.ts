326f3073e1494f10dce1f11b29c25a7b8f2c6baa
gaushao <jimiaspampa@gmail.com>
2024-11-21 07:32:58 -0300

    current-auctions-worker import json from bazaar-scraper

--- apps/current-auctions-worker/src/Data/auctions.ts
+++ apps/current-auctions-worker/src/Data/auctions.ts
@@ -1,6 +1,6 @@
 import { buildCharacterData } from 'shared-utils/dist/buildCharacterData'
-import characterData from './CurrentAuctions.json'
-import miniServerData from './ServerData.json'
+import characterData from '../../../bazaar-scraper/Output/CurrentAuctions.json'
+import miniServerData from '../../../bazaar-scraper/Output/ServerData.json'
 
 const serverArray = Object.values(
   miniServerData as Record<string, ServerObject>,
